2c6acca5f4271046a966754ba0b388e3f4792187
diorigamy <wanlewislegend@gmail.com>
2023-06-26 12:36:00 -0700

    except withoue error

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    real_no_of_elems = 0
-    for w in range(x):
+    count = 0
+
+    for i in range(x):
         try:
-            print("{:d}".format(my_list[w]), end="")
-            real_no_of_elems += 1
-        except(ValueError, TypeError):
+            print("{:d}".format(my_list[i]), end='')
+        except TypeError:
             pass
+        except ValueError:
+            pass
+        else:
+            count += 1
+
     print()
-    return real_no_of_elems
+    return (count)
