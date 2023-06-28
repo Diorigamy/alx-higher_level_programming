f778a9ca0c096f8455574c5a20259256ab6d2cdb
diorigamy <wanlewislegend@gmail.com>
2023-06-27 18:52:02 -0700

    task 5,6) classes

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,40 +1,48 @@
 #!/usr/bin/python3
-""" defining a Square class """
+"""
+module documentation
+"""
+
 
 class Square():
-    """Square class"""
+    """
+    A class that defines a square
+    """
 
     def __init__(self, size=0):
-        if type(size) != int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        else:
-        size = self.size
+
+        self.__size = size
 
     @property
-     """property decorator"""
     def size(self):
         return self.__size
 
     @size.setter
-    """setting property"""
     def size(self, value):
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        else:
-          value =  self.__size
-            
+        if value < 0:
+            raise ValueErro("size must be >= 0")
+
+        self.__size = value
+
     def area(self):
-        """calculating a squares area"""
+        """
+        calculates and returns the area of a square
+
+        Returns:
+            int: The area of the square
+        """
         return self.__size ** 2
 
     def my_print(self):
-        """print function"""
         if self.__size == 0:
             print()
+            return None
 
         for i in range(self.__size):
             for j in range(self.__size):
--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,11 +5,29 @@ module documentation
 
 
 class Square():
-    """
-    A class that defines a square
-    """
+    """A class that defines a Square"""
 
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        if self.__validate_tuple(position) is False \
+                or self.__validate_index(position) is False \
+                or self.__validate_integers(position) is False \
+                or self.__validate_value(position) is False:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
@@ -18,25 +36,42 @@ class Square():
         self.__size = size
 
     @property
-    def size(self):
-        return self.__size
+    def position(self):
+        return self.__position
 
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueErro("size must be >= 0")
+    @position.setter
+    def position(self, position):
+        if self.__validate_tuple(position) is False \
+                or self.__validate_index(position) is False \
+                or self.__validate_integers(position) is False \
+                or self.__validate_value(position) is False:
+            raise TypeError("position must be a tuple of 2 positive integers")
 
-        self.__size = value
+        self.__position = position
+
+    def __validate_tuple(self, position):
+        if type(position) is tuple:
+            return True
+        return False
+
+    def __validate_index(self, position):
+        if len(position) == 2:
+            return True
+
+        return False
+
+    def __validate_integers(self, position):
+        if type(position[0]) is int and type(position[1]) is int:
+            return True
+        return False
+
+    def __validate_value(self, position):
+        if position[0] >= 0 and position[1] >= 0:
+            return True
+        return False
 
     def area(self):
-        """
-        calculates and returns the area of a square
-
-        Returns:
-            int: The area of the square
-        """
+        """Returns the current square area"""
         return self.__size ** 2
 
     def my_print(self):
@@ -44,7 +79,14 @@ class Square():
             print()
             return None
 
-        for w in range(self.__size):
-            for x in range(self.__size):
-                print("#", end="")
-            print()
+        if self.__position[1] > 0:
+            for y in range(self.__position[1]):
+                print('')
+        for x in range(1, self.area() + 1):
+            if x % self.__size == 1:
+                print('{:>{w}}'.format('#', w=self.__position[0] + 1), end='')
+            else:
+                print('#', end='')
+
+            if x % self.__size == 0 and x > 0:
+                print()
