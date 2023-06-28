b2c45b165838a82b32b6b7976188a6a083a59199
diorigamy <wanlewislegend@gmail.com>
2023-06-27 18:11:22 -0700

    classes in python

---
+++ 0x06-python-classe/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Defining an wmpty  class """
+
+class square():
+    """empty class"""
+    pass
---
+++ 0x06-python-classe/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Moduel defining a Square class """
+
+class Square():
+    """Square class"""
+
+    def __init__(self,size):
+        """size attribute initialization below"""
+        size = self.size
